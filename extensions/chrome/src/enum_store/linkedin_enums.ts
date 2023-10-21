cf07e40cda0aa94aa82b77cd984ffe13b4f48734
harinda05 <harindaburndown@gmail.com>
2023-10-21 21:57:02 +0300

    getting the job_details from the search results to background,js

--- extensions/chrome/src/enum_store/linkedin_enums.ts
+++ extensions/chrome/src/enum_store/linkedin_enums.ts
@@ -2,3 +2,16 @@ export enum Pagetype {
     saved,
     search
 }
+
+// RuntimeCommandType Popup Background Popup
+export enum RuntimeCommandType_PBP {
+    enable_generate_event,
+    disable_generate_event,
+    generate_btn_click_event,
+    request_enable_generate_event
+}
+
+// RuntimeCommandType ContentScript Background ContentScript
+export enum RuntimeCommandType_CBC {
+    get_job_details_request_search_jobs
+}
