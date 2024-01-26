4b17e0b09285e627560b0960889f630264a63af1
harinda05 <harindaburndown@gmail.com>
2024-01-26 09:54:32 +0200

    test update

--- api/src/test/java/com/applifi/generator/api/ControllerTests.java
+++ api/src/test/java/com/applifi/generator/api/ControllerTests.java
@@ -1,5 +1,8 @@
 package com.applifi.generator.api;
 
+import com.applifi.generator.api.dto.BaseResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.applifi.generator.api.resources.ResponseTypes.REQUEST_QUEUED;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,6 +22,8 @@ public class ControllerTests {
 
     @Autowired
     MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
     @Test
     public void testPostEndpoint() throws Exception {
         MockMultipartFile file = new MockMultipartFile("resume", "file.pdf", MediaType.TEXT_PLAIN_VALUE, "Hello, World!".getBytes());
@@ -27,11 +33,12 @@ public class ControllerTests {
                         .param("description", "test_description")
                         .param("sessionId", "test")
                         .param("jobId", "test")
-                        .param("useChatGpt", "false"))
+                        .param("useChatGpt", "true"))
                 .andExpect(status().isOk())
                 .andReturn();
 
-        System.out.println(res.getResponse());
+        BaseResponse rsp_ack = objectMapper.readValue(res.getResponse().getContentAsString(),BaseResponse.class );
+        Assertions.assertEquals(REQUEST_QUEUED, rsp_ack.getResponseType());
 
     }
 
